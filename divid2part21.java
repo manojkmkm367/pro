0b444f477c079c91126a3785e0244a1214467afc
manojkmkm367 <manojkmkm367@gmail.com>
2017-08-01 10:15:44 +0530

    Create divid2part21.java

---
+++ divid2part21.java
@@ -0,0 +1,33 @@
+
+import java.util.Scanner;
+
+public class divid2part21 {
+	public static void main(String argh[])throws Exception
+	{
+	Scanner in=new Scanner(System.in);
+	System.out.println("Enter the size of array");
+	int n=in.nextInt();
+	System.out.println("Enter the elements");
+	int i,j,temp,n1,sum=0,sum1=0,s,s1;
+	int num[]=new int[10];
+	for(i=0;i<n;i++)
+	{
+		num[i]=in.nextInt();
+	}
+	n1=n/2;
+	for(i=0;i<n1;i++)
+	{
+		sum=sum+num[i];
+	}
+	for(i=n1;i<n;i++)
+	{
+		sum1=sum1+num[i];
+	}
+	s1=sum1/(n1+1); s=sum/n1;
+	if(s1==s)
+	System.out.println("Possible");
+	else
+	System.out.println("Not Possible");
+	}
+	
+}
